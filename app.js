35772827a0970d14daee95081487b87896096540
Aline Renó <aline_rissi@yahoo.com.br>
2025-03-21 15:30:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,16 @@
-function comprar() {
-    let tipo = document.getElementById('tipo-ingresso');
-    let qtd = parsenInt(document.getElementById('qtd').value);
-
-    if(tipo.value == 'pista') {
-       comprarPista(qtd);
-    } else if (tipo.value == 'superior') {
-        comprarSuperior (qtd);
-    }else {
-        comprarInferior(qtd);
+function alterarStatus (id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+  
+    if (imagem.classList.contains('dashboard__item__img--retend')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
     }
-}
 
-function comprarPista(qtd) {
-    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if (qtd > qtdPista) {
-        alert('Quantidade indisponivel para tipo pista');
-  } else {
-    qtdPista = qtdPista - qtd;
-    document.getElementById('qtd-pista').textContent = qtdPista;
-    alert('Compra realizada com sucesso!');
-  }
-}
-
-function comprarSuperior(qtd) {
-    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
-    if (qtd > qtdSuperior) {
-        alert('Quantidade indisponivel para tipo superior');
-  } else {
-    qtdSuperior = qtdSuperior - qtd;
-    document.getElementById('qtd-superior').textContent = qtdSuperior;
-    alert('Compra realizada com sucesso!');
-}
-}
-
-function comprarInferior(qtd) {
-    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
-    if (qtd > qtdInferior) {
-        alert('Quantidade indisponivel para tipo superior');
-  } else {
-    qtdInferior = qtdInferior - qtd;
-    document.getElementById('qtd-inferior').textContent = qtdInferior;
-    alert('Compra realizada com sucesso!');
-}
-}
+} 
