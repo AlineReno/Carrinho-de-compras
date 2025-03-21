4bfcbbac9825d708d6d3b5f8cd98e202e0644920
Aline Renó <aline_rissi@yahoo.com.br>
2025-03-21 14:45:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
-let totalGeral;
-limpar();
+function alterarStatus (id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+  
+    if (imagem.classList.contains('dashboard__item__img--retend')) {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
 
-function adicionar () {
-//recuperar valores nome do produto, quantidade e valor
-let produto = document.getElementById('produto').value;
-let nomeProduto = produto.split('-')[0];
-let valorUnitario = produto.split('R$')[1];
-let quantidade = document.getElementById('quantidade').value;
-//calcular o preço, o nosso subtotal
-let preco = quantidade * valorUnitario;
-let carrinho = document.getElementById('lista-produtos');
-//adicionar no carrinho
-carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-<span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$1400</span>
-</section>`;
-//atualizar o valor total
-totalGeral = totalGeral + preco;
-let campoTotal = document.getElementById('valor-total');
-campoTotal.textContent = `R$ ${totalGeral}`;
-document.getElementById('quantidade').value = 0;
-}
-
-function limpar() {
-totalGeral = 0;
-document.getElementById('lista-produtos').innerHTML = '';
-document.getElementById('valor-total').textContent = 'R$0';
-
-}
+} 
